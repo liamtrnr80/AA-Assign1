6060f933d082fb64a64030d8721712a24f96c40a
Liam <liamtrnr80@gmail.com>
2020-04-15 06:29:26 +1000

    finished orderedArray

--- OrderedArrayRQ.java
+++ OrderedArrayRQ.java
@@ -16,47 +16,47 @@ public class OrderedArrayRQ implements Runqueue {
      * Constructs empty queue
      */
     /** Head Process */
-    protected Proc pHead;
-    /** Length of array*/
-    protected int pLength;
+    protected DynamicArray array;
     
     public OrderedArrayRQ() {
-        // Implement Me
-        Proc[] processes = null;
+        array = new DynamicArray();
     }  // end of OrderedArrayRQ()
 
 
     @Override
     public void enqueue(String procLabel, int vt) {
-        // Implement me
-        Proc[] oldProcs = new Proc[pLength];
-        Proc[] newProcs = new Proc[pLength + 1];
-        
+        Proc newProc = new Proc(procLabel, vt);
+        array.add(newProc);
 
+        array.sortArray();
+        System.out.println(array.print());
     } // end of enqueue()
 
 
     @Override
     public String dequeue() {
-        // Implement me
+        Proc temp = array.getProc(0);
 
-        return ""; // placeholder,modify this
+        if(temp != null) {
+            array.remove(0);
+            return temp.getProcLabel();
+        } else {
+            return "";
+        }
     } // end of dequeue()
 
 
     @Override
     public boolean findProcess(String procLabel){
-        // Implement me
-
-        return false; // placeholder, modify this
+        return array.search(procLabel);
     } // end of findProcess()
-
 
     @Override
     public boolean removeProcess(String procLabel) {
         // Implement me
-        
-        if(true) {
+
+        if(findProcess(procLabel)) {
+            array.remove(array.find(procLabel));
             return true;
         } else {
             return false;
@@ -66,24 +66,40 @@ public class OrderedArrayRQ implements Runqueue {
 
     @Override
     public int precedingProcessTime(String procLabel) {
-        // Implement me
-
-        return -1; // placeholder, modify this
+        int time = 0;
+        if(findProcess(procLabel)) {
+            for(int i = 0; i < array.find(procLabel); i++) {
+                time += array.getProc(i).getVt();
+            }
+            return time;
+        } else {
+            return -1;
+        }
     }// end of precedingProcessTime()
 
 
     @Override
     public int succeedingProcessTime(String procLabel) {
-        // Implement me
-
-        return -1; // placeholder, modify this
+        int time = 0;
+        if(findProcess(procLabel)) {
+            for(int i = array.find(procLabel) + 1; i < array.length; i++) {
+                time += array.getProc(i).getVt();
+            }
+            return time;
+        } else {
+            return -1;
+        }
     } // end of precedingProcessTime()
 
 
     @Override
     public void printAllProcesses(PrintWriter os) {
-        //Implement me
+        StringBuffer str = new StringBuffer();
+        str = array.print();
 
+        str.setLength(str.length() - 1);
+
+        os.println(str.toString());
     } // end of printAllProcesses()
 
 } // end of class OrderedArrayRQ
@@ -101,10 +117,108 @@ class DynamicArray {
     }
     
     public void add(Proc proc) {
-        
+        if(length == cap) {
+            increaseCapacity(2);
+        }
+
         array[length] = proc;
         length++;
     }
-    
-    
+
+    public void add(int index, Proc proc) {
+        if(length == cap) {
+            increaseCapacity(2);
+        }
+
+        for(int i = length - 1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = proc;
+        length++;
+    }
+
+    public Proc getProc(int index) {
+        return array[index];
+    }
+
+    public void remove(int index) {
+        if(index >= length || index < 0) {
+            System.out.println("No element at this index");
+        } else {
+            if (length - 1 - index >= 0) System.arraycopy(array, index + 1, array, index, length - 1 - index);
+            array[length - 1] = null;
+            length--;
+            reduceCapacity();
+        }
+    }
+
+    public void increaseCapacity(int minCapacity) {
+        Proc[] temp = new Proc[cap*minCapacity];
+        if (cap >= 0) System.arraycopy(array, 0, temp, 0, cap);
+        array = temp;
+        cap = cap * minCapacity;
+
+    }
+
+    public void reduceCapacity() {
+        Proc[] temp = new Proc[length];
+        System.arraycopy(array, 0, temp, 0, length);
+        array = temp;
+        cap = array.length;
+    }
+
+    public boolean search(String index) {
+        for(int i = 0; i < length; i++) {
+            if(array[i].getProcLabel().equals(index)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(String label) {
+        for(int i = 0; i < length; i++) {
+            if(array[i].getProcLabel().equals(label)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void sortArray() {
+        boolean swapped = false;
+        int lengthRemain = length;
+
+        do {
+            swapped = false;
+            for(int i = 0; i < lengthRemain - 1; i++) {
+                if(array[i].getVt() > array[i + 1].getVt()) {
+                    Proc temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i+1] = temp;
+                    swapped = true;
+                }
+            }
+
+            lengthRemain--;
+
+        } while (swapped);
+    }
+
+    public int length(){
+        return length;
+    }
+
+    public int capacity() {
+        return cap;
+    }
+
+    public StringBuffer print() {
+        StringBuffer str = new StringBuffer();
+        for(int i = 0; i < length; i++) {
+            str.append(array[i].getProcLabel()).append(" ");
+        }
+
+        return str;
+    }
 }
